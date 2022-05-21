919dacbc9837600c8cc915e238d654c015dd4ec1
Erik <e.naaru@hotmail.com>
2022-05-21 16:21:19 +0200

    Bug fixes for networked banana growth

--- BananON/Assets/Banana/Banana.cs
+++ BananON/Assets/Banana/Banana.cs
@@ -3,27 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Banana : MonoBehaviour, IPunInstantiateMagicCallback
-{
+public class Banana : MonoBehaviour, IPunInstantiateMagicCallback {
     private float scale;
     private float growthRate;
     private MeshRenderer meshy;
     public bool isGrowing;
     public BananaSpawner spawnerObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Awake() {
+        meshy = GetComponentInChildren<MeshRenderer>();
+        spawnerObject = BananaSpawner.Instance;
+    }
 
+
+    // Start is called before the first frame update
+    void Start() {
     }
 
     public void OnPhotonInstantiate(PhotonMessageInfo info) {
         scale = 0;
+        growthRate = (float)info.photonView.InstantiationData[0];
         isGrowing = true;
         transform.localScale = new Vector3(0, 0, 0);
-        meshy = GetComponentInChildren<MeshRenderer>();
-        meshy.material.color = spawnerObject.omogenBanana;
-        growthRate = (float)info.photonView.InstantiationData[0];
+        meshy.material.color = spawnerObject.unripeBanana;
     }
 
     private void OnDestroy() {
@@ -31,15 +33,14 @@ public class Banana : MonoBehaviour, IPunInstantiateMagicCallback
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if(isGrowing) {
             scale += growthRate * Time.deltaTime;
             transform.localScale = new Vector3(scale, scale, scale);
-            meshy.material.color = Color.Lerp(spawnerObject.omogenBanana, spawnerObject.sexyBanana, scale);
+            meshy.material.color = Color.Lerp(spawnerObject.unripeBanana, spawnerObject.ripeBanana, scale);
             if(scale > 1) {
                 GetComponent<Rigidbody>().useGravity = true;
-                meshy.material.color = spawnerObject.sexyBanana;
+                meshy.material.color = spawnerObject.ripeBanana;
                 isGrowing = false;
             }
         }
--- BananON/Assets/Island/BananaTree/BananaSpawner.cs
+++ BananON/Assets/Island/BananaTree/BananaSpawner.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BananaSpawner : MonoBehaviour {
+    public static BananaSpawner Instance;
     public GameObject bananaPrefab;
     public List<GameObject> bananaPositions;
     public int bananaLimit;
     private List<Banana> spawnedBananas;
-    public Color omogenBanana;
-    public Color sexyBanana;
+    public Color unripeBanana;
+    public Color ripeBanana;
 
+    void Awake() {
+        if(Instance == null) Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start() {
@@ -33,12 +37,11 @@ public class BananaSpawner : MonoBehaviour {
     private void SpawnBanana() {
         int location = Random.Range(0, 8);
         Vector3 spawnPos = bananaPositions[location].transform.position;
-        spawnPos += new Vector3(Random.Range(0f, 1f), -1, Random.Range(0f, 1f));
+        spawnPos += new Vector3(Random.Range(0f, 1f), 0, Random.Range(0f, 1f));
         Quaternion spawnRotation = Quaternion.Euler(0, Random.Range(0f, 360f), -90f);
-        object[] spawnParams = { Random.Range(0.02f, 0.05f) };
+        object[] spawnParams = { Random.Range(0.02f, 0.05f)};
         GameObject bananaObj = PhotonNetwork.Instantiate("Banana", spawnPos, spawnRotation, data:spawnParams);
         Banana banana = bananaObj.GetComponent<Banana>();
-        banana.spawnerObject = this;
         spawnedBananas.Add(banana);
     }
 }
